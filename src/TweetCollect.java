aff8affba74c7cb5ab750f21135c2a01e29169b2
Jiaying Yu <jiaying.yu@nyu.edu>
2015-04-28 14:11:09 -0400

    write to file

--- src/TweetCollect.java
+++ src/TweetCollect.java
@@ -1,16 +1,28 @@
-
 // add twitter4j-core-4.0.3.jar to classpath
 //This program collects 200 tweets based on the Query object
 
-import twitter4j.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
+
 public class TweetCollect {
-	public static void main(String[] args) throws TwitterException {
-		final String CONSUMER_KEY = "kfWvjLrk9pV9fJBwBmPq6zQz8";
-		final String CONSUMER_SECRET = "eBCpt1JZHtzY3EVzOk8GRcmVA99oVo6la8mKRMwrYdLhoaYGQ2";
-		final String ACCESS_TOKEN = "3003809134-O726b4UnkM4tDxMECl2KuLpsHBIbD981cgycsT3";
-		final String ACCESS_TOKEN_SECRET = "YsPxVNzqhWBy4leGdZLmBYWI6VifnB2R6LfECz3CuD6kI";
-		
+	private static final String CONSUMER_KEY = "kfWvjLrk9pV9fJBwBmPq6zQz8";
+	private static final String CONSUMER_SECRET = "eBCpt1JZHtzY3EVzOk8GRcmVA99oVo6la8mKRMwrYdLhoaYGQ2";
+	private static final String ACCESS_TOKEN = "3003809134-O726b4UnkM4tDxMECl2KuLpsHBIbD981cgycsT3";
+	private static final String ACCESS_TOKEN_SECRET = "YsPxVNzqhWBy4leGdZLmBYWI6VifnB2R6LfECz3CuD6kI";
+	private static BufferedWriter bw; //write results into a file
+	private static Twitter twitter;
+	public static void main(String[] args) throws TwitterException {		
+		//set up configuration
 		ConfigurationBuilder cb = new ConfigurationBuilder();
 		cb.setDebugEnabled(true);
 		cb.setOAuthConsumerKey(CONSUMER_KEY);
@@ -18,25 +30,57 @@ public class TweetCollect {
 		cb.setOAuthAccessToken(ACCESS_TOKEN);
 		cb.setOAuthAccessTokenSecret(ACCESS_TOKEN_SECRET);
 		
-		Twitter twitter = new TwitterFactory(cb.build()).getInstance();
-	  Query query = new Query("movie");
-	  query.setCount(100);
-	  System.out.println(query.getCount());
-	  QueryResult result = twitter.search(query);
-	  for (Status status : result.getTweets()) {
-	      System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
-	  }
-	  
-	  if (result.hasNext()) {
-	  	query = result.nextQuery();
-	  }
-	  
-	  result = twitter.search(query);
-	  
-	  for (Status status : result.getTweets()) {
-	      System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
-	  }
-	  
+		//create buffered writer to write results into dataset file
+		try {
+			File out = new File("tweets_dataset.txt");
+			if (out.exists()) {
+				out.createNewFile();
+			}
+			
+			FileWriter fw = new FileWriter(out.getAbsoluteFile());
+			bw = new BufferedWriter(fw);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		twitter = new TwitterFactory(cb.build()).getInstance();
+		Query query = new Query("movie");
+		query.setCount(100); 	// set number of tweets returned per page to 100
+
+		writeToFile(query);
+		System.out.println("Finish collect");
+		//printSearchResultsPerPage(result);
+		//if there is next page print next page
+//		if (result.hasNext()) {
+//			query = result.nextQuery();
+//			result = twitter.search(query);
+//		}
+//		printSearchResultsPerPage(result);
+		
+		//close buffered writer
+		try {
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void printSearchResultsPerPage(QueryResult result) {
+		for (Status status : result.getTweets()) {
+			System.out.println("@" + status.getUser().getScreenName() + ":"
+					+ status.getText());
+		}
+	}
+	
+	public static void writeToFile(Query query) throws TwitterException {
+		QueryResult result = twitter.search(query);
+		for (Status status: result.getTweets()) {
+			String content = status.getText();
+			try {
+				bw.write(content + "\n");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
-	
